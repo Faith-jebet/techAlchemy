a7d19bc01cb8aeb837f0f31dde5e973c33064ed9
Faith-jebet <kipronojebet5gmail.com>
2025-02-17 15:02:10 +0300

    first commit

---
+++ techalchemy/app/index.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import Login from "./components/Login";
+
+export default function Index() {
+  return (
+    <View>
+      <Text>Hello World.</Text>
+      <Login />
+    </View>
+  );
+}
