9a7b30f09e0db7a43758804da01a49bfaf284173
Tornshorts <kawewaalex@gmail.com>
2025-02-19 09:36:22 +0300

    login

--- techalchemy/app/_layout.tsx
+++ techalchemy/app/_layout.tsx
@@ -1,5 +1,20 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerBackTitle: "Home",
+        }}
+      />
+      <Stack.Screen
+        name="Login"
+        options={{
+          headerBackTitle: "Login",
+        }}
+      />
+    </Stack>
+  );
 }
--- techalchemy/app/index.jsx
+++ techalchemy/app/index.jsx
@@ -1,11 +1,14 @@
-import { Text, View } from "react-native";
-import Login from "./components/Login";
+import { Text, View, Button } from "react-native";
+import Login from "./Login";
+import { Link } from "expo-router";
+import React from "react";
+
 
 export default function Index() {
   return (
     <View>
       <Text>Hello World.</Text>
-      <Login />
+      <Link href="/Login">Login</Link>
     </View>
   );
 }
